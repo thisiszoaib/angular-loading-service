b525d270537d233e858b8e8238c09da88e61f473
Zoaib Ahmed Khan <thisiszoaib@gmail.com>
2021-10-26 12:07:33 +0500

    Fix for ExpressionChanged Error

--- src/app/loading.service.ts
+++ src/app/loading.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoadingService {
   private _loading = new BehaviorSubject<boolean>(false);
-  public readonly loading$ = this._loading.asObservable();
+  public readonly loading$ = this._loading.asObservable().pipe(delay(1));
 
   constructor() {}
 
